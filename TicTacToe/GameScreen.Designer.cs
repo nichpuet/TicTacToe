2603cb444d63852425811df66d014441484c8120
Nick Puetz <Napez702@hotmail.com>
2019-03-08 09:59:47 -0500

    i cant win

--- TicTacToe/GameScreen.Designer.cs
+++ TicTacToe/GameScreen.Designer.cs
@@ -28,22 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.winLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(150, 0);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(35, 13);
+            this.winLabel.TabIndex = 0;
+            this.winLabel.Text = "label1";
+            this.winLabel.Visible = false;
             // 
             // GameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BurlyWood;
+            this.Controls.Add(this.winLabel);
             this.Name = "GameScreen";
             this.Size = new System.Drawing.Size(350, 350);
             this.Load += new System.EventHandler(this.GameScreen_Load);
             this.Click += new System.EventHandler(this.GameScreen_Click);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameScreen_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label winLabel;
     }
 }
--- TicTacToe/GameScreen.cs
+++ TicTacToe/GameScreen.cs
@@ -92,30 +92,29 @@ namespace TicTacToe
                 if (blankTiles[i].x == tempX && blankTiles[i].y == tempY)
                 {
                     blankTiles.RemoveAt(i);
-                }
-            }
 
-            
-            if (p1Turn == true)
-            {
-                xTiles.Add(playerTile);
-                p1Turn = false;
-                foreach(Tiles t in xTiles)
-                {
-                    t.drawTile(t.x, t.y, p1Turn, scale);
+                    if (p1Turn == true)
+                    {
+                        xTiles.Add(playerTile);
+                        p1Turn = false;
+                        foreach (Tiles t in xTiles)
+                        {
+                            t.drawTile(t.x, t.y, p1Turn, scale);
+                        }
+
+                    }
+                    else if (p1Turn == false)
+                    {
+                        oTiles.Add(playerTile);
+                        p1Turn = true;
+                        foreach (Tiles l in oTiles)
+                        {
+                            l.drawTile(l.x, l.y, p1Turn, scale);
+                        }
+                    }
                 }
-                
             }
-            else if (p1Turn == false)
-            {
-                oTiles.Add(playerTile);
-                p1Turn = true;
-                foreach(Tiles l in oTiles)
-                {
-                    l.drawTile(l.x, l.y, p1Turn, scale);
-                }
-            }
-                      
+            WinCheck(p1Turn);
             Refresh();
         }
 
@@ -136,8 +135,40 @@ namespace TicTacToe
                 Tiles blankTile = new Tiles((15+(50 * i))*scale, 115*scale, true);
                 blankTiles.Add(blankTile);
             }
+        }
 
-            
+        private void WinCheck(bool pTurn)
+        {
+            int counterX = 0, counterO = 0;
+
+            for(int i = 0; i < xTiles.Count(); i++)
+            {
+                if (xTiles[i].x == 30 && pTurn == true)
+                {
+                    counterX++;
+                }
+
+                if (counterX == 3)
+                {
+                    winLabel.Text = "X's Win";
+                    winLabel.Visible = true;
+                    counterX = 0;
+                }
+            }
+            for(int i = 0;i < oTiles.Count(); i++)
+            {
+                if (oTiles[i].x == 30 && pTurn == false)
+                {
+                    counterO++;
+                }
+
+                if (counterO == 3)
+                {
+                    winLabel.Text = "O's Win";
+                    winLabel.Visible = true;
+                    counterX = 0;
+                }
+            }
         }
     }
 }
